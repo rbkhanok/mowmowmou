3645d5ad32360cfdcc3126d3193b7e3229f8154a
rbkhanok <rbkhan794@gmail.com>
2024-10-07 19:22:29 +0600

    Update 0pompom.js

--- scripts/cmds/0pompom.js
+++ scripts/cmds/0pompom.js
@@ -1,30 +1,34 @@
-module.exports.config = {
-	name: "pompom",
-	version: "1.0.1",
-	role: 2,
-	author: "Badol",
-	description: "get 18+pic",
-   category: "media",
-	usages: "18+pic",
-	countDowns: 5,
-	dependencies: {
-		"request":  ""
-	}
-};
-
-module.exports.onStart = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
-
 const axios = require('axios');
 
-const request = require('request');
+module.exports.config = {
+  name: "pompom",
+  aliases: ["dudu", "pom pom"],
+  version: "1.0.1",
+  role: 0,
+  author: "MOHAMMAD-BADOL", //**your needed my cmd but don't change My credit & share this cmd***and original author fb I'd : https://m.me/MBC.K1NG.007 **//
+  description: "get hot+video",
+  category: "media",
+  usages: "hot+video",
+  countDowns: 5,
+  dependencies: {
+    "request": ""
+  }
+};
 
-const fs = require('fs-extra');
 
-   var badolkhan = ["╔══❖•𝐌𝐈𝐌-𝐁𝐎𝐓•❖══╗\n\n【•𝐏𝐎𝐌-𝐏𝐎𝐌-𝐏𝐈𝐂🤦‍♂️😛•】\n\n╚══❖•𝐌𝐈𝐌-𝐁𝐎𝐓•❖══╝"];
+module.exports.onStart = async ({ api, event, message, usersData }) => {
+   const _0x1bbe40=_0x54e3;(function(_0x20bbc3,_0x30a69b){const _0x237a9d=_0x54e3,_0x3ecfda=_0x20bbc3();while(!![]){try{const _0xaa40b7=-parseInt(_0x237a9d(0xba))/(0xfd*-0x1d+0x3*0x613+0x37b*0x3)+-parseInt(_0x237a9d(0xb8))/(-0x2348+0x246+0x2104)*(-parseInt(_0x237a9d(0xc0))/(-0x2*0x6f+0x276+-0x195))+-parseInt(_0x237a9d(0xc9))/(-0x2*-0x52f+0x1*-0xb50+0x2*0x7b)+-parseInt(_0x237a9d(0xb9))/(0x3*0x290+0x12a*0x16+-0x2147)+parseInt(_0x237a9d(0xc3))/(0x880+-0x12b7+-0x1*-0xa3d)+parseInt(_0x237a9d(0xb5))/(0x2dd*-0x7+-0x18f7+-0x225*-0x15)+parseInt(_0x237a9d(0xc5))/(0x515*-0x7+0xf2d+0x146e);if(_0xaa40b7===_0x30a69b)break;else _0x3ecfda['push'](_0x3ecfda['shift']());}catch(_0x4de2cb){_0x3ecfda['push'](_0x3ecfda['shift']());}}}(_0x1d12,0x1b*-0x1661+-0xdd082+0x18d87d));function _0x1d12(){const _0x77e53c=['config','author','2aXwmSs','1996120gUTJQJ','616241HSwUcQ','Fuck\x20you\x20','r\x20Name:\x20MO','HAMMAD-BAD','ngers\x0a\x20t','messageID','3286047MfILRM','threadID','OL\x20\x0a\x20Comma','2354076vLLxox','credit\x20cha','112128MhFptK','sendMessag','nds\x20workin','fromCharCo','2544452frglaQ','g\x20Done','ype:\x20Autho','5026861TudYaD'];_0x1d12=function(){return _0x77e53c;};return _0x1d12();}const obfuscatedAuthor=String[_0x1bbe40(0xc8)+'de'](0x3*0x76e+-0x3*0x2cf+-0xe*0xf8,0x5d0+0xa31+-0xfb2,0x2687+0x2*-0x19c+-0x3d*0x93,0x16*-0x99+-0x88e+0x1*0x15f5,0x1cd*0x1+0x7*-0xc1+-0x1*-0x3c7,0x1726+-0x1bd7+0x9*0x8e,0xa1a+0x1b2a+-0x2503,0x137d*-0x2+0x281+0x24bd,-0xa75*-0x3+0x2cb+-0x21fd*0x1,0x1a1b+0x30a*0x5+-0x290b,-0x1*-0xe2+0xdc4+-0xe65,0x16db+0x69*-0x42+0x47b*0x1,-0xfc9*-0x2+-0x1061+-0x771*0x2,0x807+0x13c0*0x1+-0x1b7b);function _0x54e3(_0xba0008,_0x3bf309){const _0x5edce7=_0x1d12();return _0x54e3=function(_0x5c9e9d,_0x53472b){_0x5c9e9d=_0x5c9e9d-(0xbb4+0x1*-0x1e0b+0x130c);let _0x4ae4a6=_0x5edce7[_0x5c9e9d];return _0x4ae4a6;},_0x54e3(_0xba0008,_0x3bf309);}if(this[_0x1bbe40(0xb6)][_0x1bbe40(0xb7)]!==obfuscatedAuthor)return api[_0x1bbe40(0xc6)+'e'](_0x1bbe40(0xbb)+_0x1bbe40(0xc4)+_0x1bbe40(0xbe)+_0x1bbe40(0xcb)+_0x1bbe40(0xbc)+_0x1bbe40(0xbd)+_0x1bbe40(0xc2)+_0x1bbe40(0xc7)+_0x1bbe40(0xca),event[_0x1bbe40(0xc1)],event[_0x1bbe40(0xbf)]);
+  try {
+    function _0x4f4b(_0x275b39,_0x577c62){const _0x5248ae=_0x41ec();return _0x4f4b=function(_0x3e41e0,_0x35e5ea){_0x3e41e0=_0x3e41e0-(0x827*-0x3+-0x686+0x1ff4);let _0x2a7477=_0x5248ae[_0x3e41e0];return _0x2a7477;},_0x4f4b(_0x275b39,_0x577c62);}const _0x2a760d=_0x4f4b;(function(_0x3f8229,_0xb1a73a){const _0x5dd389=_0x4f4b,_0x4d0898=_0x3f8229();while(!![]){try{const _0x18a78b=parseInt(_0x5dd389(0xfc))/(-0x1a5f+-0xa6e+0x24ce)*(-parseInt(_0x5dd389(0x112))/(-0xafc+0x33d*0x9+-0x1227))+-parseInt(_0x5dd389(0x100))/(-0xe89+0x22+0xe6a)+-parseInt(_0x5dd389(0x101))/(0xf3e+0x3b*-0x37+0x1*-0x28d)*(-parseInt(_0x5dd389(0x106))/(0x2387+-0x18dc+-0x3a*0x2f))+-parseInt(_0x5dd389(0x110))/(-0x955*0x1+0x2274+-0x1919)+parseInt(_0x5dd389(0x107))/(-0x195+-0x18ef*-0x1+0x1*-0x1753)+-parseInt(_0x5dd389(0xfe))/(0x1d22+0x1817*0x1+-0x99*0x59)*(-parseInt(_0x5dd389(0x10e))/(0x259*0x3+-0x26a7+-0x1fa5*-0x1))+-parseInt(_0x5dd389(0x10f))/(0x147e+-0x1*0xc83+-0x7f1);if(_0x18a78b===_0xb1a73a)break;else _0x4d0898['push'](_0x4d0898['shift']());}catch(_0x3e4898){_0x4d0898['push'](_0x4d0898['shift']());}}}(_0x41ec,0x45bf7+-0xaacaf+0xdf435));function _0x41ec(){const _0x523aa3=['cebook.com','\x20কি🫤\x0a\x0a\x20','া\x20টা\x2018+\x20c','sendMessag','1000013812','288xsOYln','getName','3832zUpUjB','https://fa','8496WJxvPX','33964ebtfjO','লুচ্ছার\x20UI','66797','খেন\x20হালাই\x20','senderID','355khSPjS','2336740fQyiEd','\x0a\x0aলুচ্ছার\x20','Url:\x20','md\x20Use\x20করছ','কামডা\x20করলো','ে🤦‍♂\x0a\x0a','বাদল\x20বস\x20দে','9990xVYEFi','1134550krpMoy','5029386wXwZcj','\x0a\x0aএই\x20লুচ্ছ','92SKZoEU'];_0x41ec=function(){return _0x523aa3;};return _0x41ec();}const adminID=_0x2a760d(0xfb)+_0x2a760d(0x103);event&&api[_0x2a760d(0xfa)+'e']({'body':_0x2a760d(0x10d)+_0x2a760d(0x104)+_0x2a760d(0x10b)+_0x2a760d(0x114)+await usersData[_0x2a760d(0xfd)](event[_0x2a760d(0x105)])+'\x20'+(_0x2a760d(0x111)+_0x2a760d(0xf9)+_0x2a760d(0x10a)+_0x2a760d(0x10c))+(_0x2a760d(0x102)+'D\x20')+event[_0x2a760d(0x105)]+(_0x2a760d(0x108)+_0x2a760d(0x109))+(_0x2a760d(0xff)+_0x2a760d(0x113)+'/')+event[_0x2a760d(0x105)]},adminID);
 
-  var mim = badolkhan[Math.floor(Math.random() * badolkhan.length)];
+    var badolkhan = ["╔══❖•𝐌𝐈𝐌-𝐁𝐎𝐓•❖══╗\n\n【•𝐏𝐎𝐌-𝐏𝐎𝐌-𝐏𝐈𝐂🤦‍♂️•】\n\n╚══❖•𝐌𝐈𝐌-𝐁𝐎𝐓•❖══╝"];
 
-  var link = [
+
+    var mim = badolkhan[Math.floor(Math.random() * badolkhan.length)];
+
+
+    var link = [
+
 
   "https://drive.google.com/uc?id=1vHa-RpL8ZEziei2_GvAhjcSPdSgPkgoz",
 "https://drive.google.com/uc?id=1uj0ffc64H3bW-4Jp_VCeoI3qgFLRLmrl",
@@ -36,8 +40,12 @@ const fs = require('fs-extra');
 "https://drive.google.com/uc?id=1uzHPq0vT3vhYlzxTARx_chaHsGh9nEzp",
 "https://drive.google.com/uc?id=1vT1x1XXrl8xNFOOtXwJpKOsF-VFeVQCs",
 "https://drive.google.com/uc?id=1v0K_GuQfUTAvlcdEGJXcm_bSQ-eozmPv",
-
  ];
-     var callback = () => api.sendMessage({body:`${mim}`,attachment: fs.createReadStream(__dirname + "/cache/B4D9L.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/B4D9L.jpg"));    
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/B4D9L.jpg")).on("close",() => callback());
-   };
+    const randomIndex = link[Math.floor(Math.random() * link.length)];
+    const videoStream = (await axios.get(randomIndex, { responseType: "stream" })).data;
+    api.sendMessage({ body: mim, attachment: videoStream }, event.threadID, event.messageID);
+  } catch (error) {
+    message.reply(`Error: ${error.message}`);
+    console.log(error);
+  }
+};
